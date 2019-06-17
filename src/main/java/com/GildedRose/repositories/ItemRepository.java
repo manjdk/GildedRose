7e1aa1d3b9336f5295cc27b50252141853ad683f
Mantas <manjdk@telesoftas.com>
2019-06-17 23:51:11 +0300

    feat: Add ItemRepository with findAll() function

---
+++ src/main/java/com/GildedRose/repositories/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.GildedRose.repositories;
+
+import com.GildedRose.Item;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends MongoRepository<Item, String> {
+    List<Item> findAll();
+}
