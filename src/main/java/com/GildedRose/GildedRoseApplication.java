25d3faa5a1d15e791089e4eaf59ee09ac0c3d2a7
Mantas <manjdk@telesoftas.com>
2019-06-17 23:50:24 +0300

    feat: Chore application start, load initial data to repository

--- src/main/java/com/GildedRose/GildedRoseApplication.java
+++ src/main/java/com/GildedRose/GildedRoseApplication.java
@@ -1,11 +1,38 @@
 package com.GildedRose;
 
+import com.GildedRose.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GildedRoseApplication {
+public class GildedRoseApplication implements CommandLineRunner {
+    @Autowired
+    private ItemRepository itemRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(GildedRoseApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args){
+        itemRepository.deleteAll();
+
+        Item[] initialItems = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Conjured Mana Cake", 3, 6)
+        };
+
+        for (Item item : initialItems) {
+            itemRepository.save(item);
+        }
     }
 }
