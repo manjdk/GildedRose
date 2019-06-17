181880d097cb36c16d31d9d35cd6b98cb88f7cca
Mantas <manjdk@telesoftas.com>
2019-06-17 23:55:59 +0300

    feat: Add items update by its schedule

--- src/main/java/com/GildedRose/GildedRose.java
+++ src/main/java/com/GildedRose/GildedRose.java
@@ -1,20 +1,49 @@
 package com.GildedRose;
 
 import com.GildedRose.items.UpdateItem;
+import com.GildedRose.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+@Component
+@EnableScheduling
+@Configuration
 class GildedRose {
     Item[] items;
     private Category category;
+    private int daysCounter;
+    private Logger logger;
+
+    @Autowired
+    private ItemRepository itemRepository;
 
     public GildedRose(Item[] items) {
         this.items = items;
         this.category = new Category();
+        this.daysCounter = 0;
+        this.logger = Logger.getLogger(String.valueOf(GildedRose.class));
     }
 
+    @Scheduled(initialDelayString = "${delay.initial}", fixedDelayString = "${delay.fixed}")
     public void updateQuality() {
-        for (Item item : items) {
+        logger.info("-------- day " + daysCounter + " --------");
+        logger.info("id, name, sellIn, quality");
+
+        List<Item> all = itemRepository.findAll();
+
+        for (Item item : all) {
             UpdateItem selectedCategory = category.defineCategory(item);
             selectedCategory.updateItem(item);
+            logger.info(String.valueOf(item));
+            itemRepository.save(item);
         }
+
+        daysCounter++;
     }
 }
